900277c066382e018962587e7f2c063cf8ba52e4
akoidan <deathangel908@gmail.com>
2020-03-28 03:02:05 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="mysql-server-has-gone-away",
+    version="1.0.0",
+    description="Django myslq backend that fixes issue with long living connection",
+    author='Andrew Koidan <deathangel908@gmail.com>',
+    license="MIT",
+)
